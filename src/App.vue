4a9c0e6e86c293714eb75643a29107b44960c100
weilengdezaoshang <910843376@qq.com>
2022-11-06 11:24:48 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">这是一线通生活项目</div>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+html,
+body,
+#app 
+.container {
+  height: 100%;
+}
+</style>
